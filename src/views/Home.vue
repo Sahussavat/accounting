0b4a1c141ab0dda92e567d2932fd39bdc322536a
Sahussavat Konrama <mrkekeon2@gmail.com>
2021-07-25 02:40:21 +0700

    Prototype 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AccoutingTable></AccoutingTable>
+    <AccoutingForm></AccoutingForm>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import AccoutingTable from '@/components/AccountingTable.vue'
+import AccoutingForm from '@/components/AccountingForm.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    AccoutingTable,
+    AccoutingForm
   }
 }
 </script>
